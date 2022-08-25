8e7bad56d4f1db08811de5e7a4eec44dc1b9c7e8
iamageo <geovani.af4@gmail.com>
2022-08-25 16:38:09 -0300

    Initial commit 🏁

---
+++ app/src/main/java/com/iamageo/multifab/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.iamageo.multifab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.iamageo.multifab.ui.theme.MultiFabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MultiFabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
