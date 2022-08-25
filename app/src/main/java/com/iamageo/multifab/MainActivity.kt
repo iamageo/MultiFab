5dbd65e9ece07566302ab71d842c275817d3b2ac
iamageo <geovani.af4@gmail.com>
2022-08-25 17:19:46 -0300

    testing multifab 😍

--- app/src/main/java/com/iamageo/multifab/MainActivity.kt
+++ app/src/main/java/com/iamageo/multifab/MainActivity.kt
@@ -3,26 +3,51 @@ package com.iamageo.multifab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material.Scaffold
+import androidx.compose.ui.graphics.Color
 import com.iamageo.multifab.ui.theme.MultiFabTheme
+import com.iamageo.multifablibrary.FabIcon
+import com.iamageo.multifablibrary.FabOption
+import com.iamageo.multifablibrary.MultiFabItem
+import com.iamageo.multifablibrary.MultiFloatingActionButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MultiFabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Scaffold(
+                    backgroundColor = Color.Black,
+                    floatingActionButton = {
+                        MultiFloatingActionButton(
+                            fabIcon = FabIcon(
+                                iconRes = R.drawable.ic_baseline_add_24,
+                                iconResAfterRotate = R.drawable.ic_baseline_remove_24,
+                                iconRotate = 180f
+                            ),
+                            fabOption = FabOption(
+                                iconTint = Color.White,
+                                showLabels = true,
+                                backgroundTint = Color.Blue,
+                            ),
+                            itemsMultiFab = listOf(
+                                MultiFabItem(
+                                    icon = R.drawable.ic_baseline_add_24,
+                                    label = "first",
+                                    labelcolor = Color.White
+                                ),
+                                MultiFabItem(
+                                    icon = R.drawable.ic_baseline_add_24,
+                                    label = "second",
+                                    labelcolor = Color.White
+                                ),
+                            ),
+                            onFabItemClicked = { println(it) },
+                            fabTitle = "MultiFloatActionButton",
+                            showFabTitle = false
+                        )
+                    }
                 ) {
-
                 }
             }
         }
